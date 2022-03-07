096801ffa0c6789beb1f3e60a46fc9c40e0e6bec
Dalia Farag <daliamfarag88@gmail.com>
2022-03-07 01:18:39 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize")
 
 require("dotenv").config()
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
